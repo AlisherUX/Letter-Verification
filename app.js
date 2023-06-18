829ef639dbd3b2cdab677742556f389870462a9c
alisher <abdirimov110@gmail.com>
2023-06-18 16:29:56 +0500

    alisher

--- app.js
+++ app.js
@@ -1,30 +1,63 @@
-//---> JSda ma'lumotlarni tekshirish <---//
-// Bu mavzuda siz ma'lumotni Stringga, Numberga va Booleanga tekshirishni o'rganib olasiz.
+//---> JSda harflarni tekshirish <---//
+// Bu mavzuda siz harflarni katta yoki kichikligini tekshirishni o'rganib olasiz.
 
 //Global variables
 //\/\/\/\/\/\/\/\/
-let str = "alisher";
-let num = 17;
-let bool = true;
+let big = "A";
+let small ="a";
+let mixed = "Aa";
 
-//1) String verification
-console.log(  typeof str === 'string',  ); //way---1  return: true
-console.log(  str.includes(''),  );        //way---2  return: true
-console.log(  typeof str,  );              //way---3  return: string
+//1) Big letter verification
+console.log(  /[A-Z]/.test(big)  ); //way---1  return: true
+console.log(  /[A-Z]/.test(small)  );//        return: false
+console.log(  /[A-Z]/.test(mixed)  );//        return: true
+//Description
+// Bu funksiya katta harflarni qidiradi agar faqat 1ta katta harf topsa ham true qaytaradi,
+// masalan: big o'zgaruvchisi o'rniga mixed o'zgaruvchisi qo'yilsa ham funksiya true qaytaradi. 
 
-//2) Number verification
-console.log(  typeof num === 'number'  ); //way---1  return: true
-console.log(  Number.isInteger(num)  );   //way---2  return: true
-console.log(  typeof num  );              //way---3  return: number
 
-//3) Boolean verification
-console.log(  typeof bool === 'boolean'  );//way---1  return: true
-console.log(  typeof bool  );              //way---2  return: boolean
-console.log(  bool === true || bool === false  );//way---3  return: true
 
-//Conclusion <==translate==> Xulosa
+console.log(  /[A-Z]+$/.test(big)  ); //way---2  return: true
+console.log(  /[A-Z]+$/.test(small)  );//          return: false
+console.log(  /[A-Z]+$/.test(mixed)  );//          return: false
+//Description
+// Bu funksiya true qaytarishi uchun barcha harflar katta bo'lishi kerak,
+// Agar big o'zgaruvchisi o'rniga mixed o'zgaruvchisi qo'yilsa funksiya false qaytaradi. 
 
-//Bu ikkiala funksiyani barcha ma'lumot turlari uchun ishlatsa bo'ladi.
-// \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/
-//`<1.>(typeof var === 'data type')
-//<2.>(typeof var)
+
+
+console.log(  Boolean(big.match(/[A-Z]/))  ); //way---3  return: true
+console.log(  Boolean(small.match(/[A-Z]/))  );//          return: false
+console.log(  Boolean(mixed.match(/[A-Z]/))  );//          return: true
+//Description
+// Bu funksiya birinchi funksiya bilan bir xil ishlaydi.
+
+
+
+
+
+
+//2) Small letter verification
+console.log(  /[a-z]/.test(small)  ); //way---1  return: true
+console.log(  /[a-z]/.test(big)  );//        return: false
+console.log(  /[a-z]/.test(mixed)  );//        return: true
+//Description
+// Bu funksiya kichik harflarni qidiradi agar faqat 1ta kichik harf topsa ham true qaytaradi,
+// masalan: small o'zgaruvchisi o'rniga mixed o'zgaruvchisi qo'yilsa ham funksiya true qaytaradi. 
+
+
+
+console.log(  /[a-z]+$/.test(small)  ); //way---2  return: true
+console.log(  /[a-z]+$/.test(big)  );//          return: false
+console.log(  /[a-z]+$/.test(mixed)  );//          return: false
+//Description
+// Bu funksiya true qaytarishi uchun barcha harflar kichik bo'lishi kerak,
+// Agar small o'zgaruvchisi o'rniga mixed o'zgaruvchisi qo'yilsa funksiya false qaytaradi. 
+
+
+
+console.log(  Boolean(small.match(/[a-z]/))  ); //way---3  return: true
+console.log(  Boolean(big.match(/[a-z]/))  );//          return: false
+console.log(  Boolean(mixed.match(/[a-z]/))  );//          return: true
+//Description
+// Bu funksiya to'rtinchi funksiya bilan bir xil ishlaydi.
